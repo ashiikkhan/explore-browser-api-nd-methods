895fad0669ecb12531321777f3e8f5174170cc88
ashiikkhan <ashiksust06@gmail.com>
2022-09-12 15:35:10 +0600

    callback function

--- commonjs/callback.js
+++ commonjs/callback.js
@@ -1,5 +1,20 @@
-function greetings(person) {
-  console.log(`hello, ${person}`);
+/**
+ * A callback function is a function passed as an argument to another function.
+ * This technique allows a function to call another function.
+ * A callback function can run after another function has finished.
+ */
+function greeting(greetingHandler, person) {
+  greetingHandler(person);
 }
-const person = 'ashik';
-console.log(greetings(person));
+const person1 = 'mr. lazy';
+const person2 = 'mr. programmer';
+function morningGreeting(person) {
+  console.log('good morning', person);
+}
+function eveningGreeting(person) {
+  console.log('good evening', person);
+}
+greeting(morningGreeting, person1);
+greeting(morningGreeting, person2);
+greeting(eveningGreeting, person1);
+greeting(eveningGreeting, person2);
